384682e8186bbb15404f20aeae24e7c5a7e2f1c3
Mike Clancy <mclancy96@gmail.com>
2024-11-03 22:06:07 -0500

    just some other things to try

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,61 @@
-const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
-
-  return {
-    mode: isProduction ? "production" : "development",
-    entry: {
-      application: "./app/javascript/packs/application.js"
-    },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(__dirname, "public/packs"),
-      publicPath: "/packs/"
-    },
-    resolve: {
-      extensions: [".js", ".jsx", ".scss", ".css"]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"]
-            }
-          }
+module.exports = {
+  entry: './app/javascript/packs/application.js', // Adjust the entry point as needed
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/packs'), // Adjust the output path as needed
+    publicPath: '/packs/',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
         },
-        {
-          test: /\.(scss|css)$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader",
-            {
-              loader: "postcss-loader",
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    require("autoprefixer")
-                  ]
-                }
-              }
-            },
-            "sass-loader"
-          ]
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
-          type: "asset/resource",
-          generator: {
-            filename: "images/[hash][ext][query]"
-          }
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(), // Cleans output directory on each build
-      new MiniCssExtractPlugin({
-        filename: "[name].css"
-      }),
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        Popper: ["popper.js", "default"]
-      })
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ico)$/, // Add more file types if needed
+        type: 'asset/resource', // Updated for Webpack 5
+      },
+      {
+        test: /\.json$/,
+        type: 'json', // Ensures that JSON files are handled correctly
+      },
     ],
-    devServer: {
-      hot: true,
-      contentBase: path.resolve(__dirname, "public/packs"),
-      publicPath: "/packs/",
-      port: 3035,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    },
-    devtool: isProduction ? "source-map" : "eval-source-map"
-  };
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'], // Add other extensions as needed
+  },
+  devtool: 'source-map', // Optional for debugging
+
 };

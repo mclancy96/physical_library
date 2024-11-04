f7e689de05a73023fa7b6cd7f2156ff9c4719ab5
Mike Clancy <mclancy96@gmail.com>
2024-11-03 20:37:46 -0500

    attempt to fix application.js part 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,83 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: {
+      application: "./app/javascript/packs/application.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "public/packs"),
+      publicPath: "/packs/"
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".scss", ".css"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    require("autoprefixer")
+                  ]
+                }
+              }
+            },
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[hash][ext][query]"
+          }
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(), // Cleans output directory on each build
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        Popper: ["popper.js", "default"]
+      })
+    ],
+    devServer: {
+      hot: true,
+      contentBase: path.resolve(__dirname, "public/packs"),
+      publicPath: "/packs/",
+      port: 3035,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map"
+  };
+};

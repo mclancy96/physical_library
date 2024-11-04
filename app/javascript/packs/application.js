02c23c0628199bf7c4c6645d97b51042ed71fa56
Mike Clancy <mclancy96@gmail.com>
2024-11-04 11:57:48 -0500

    testing removing build command

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,16 @@
 import "bootstrap";
  import '@popperjs/core';
 import "../../assets/stylesheets/application.scss"; // If you have stylesheets, ensure this is correct
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
+// Import your custom JavaScript files
+import "./controllers/toast";
+import "./controllers/scan_book";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 // Your additional JavaScript code here

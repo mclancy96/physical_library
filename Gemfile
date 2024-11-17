16b9526c6f752e187a78d4356f9cc760d5c50678
Mike Clancy <mclancy96@gmail.com>
2024-11-17 13:21:26 -0500

    pushing to test dewey decimal feasibility

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'image_processing'
 
 gem 'ruby-vips'
 
+gem 'httparty'
+gem 'nokogiri'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ app/services/librarything_scraper.rb
@@ -0,0 +1,26 @@
+require 'httparty'
+require 'nokogiri'
+
+class LibrarythingScraper
+  BASE_URL = 'https://www.librarything.com/isbn/'.freeze
+
+  def self.scrape(isbn)
+    url = "#{BASE_URL}#{isbn}"
+    puts "URL: #{url}"
+    response = HTTParty.get(url, headers: { 'User-Agent' => 'Mozilla/5.0' })
+
+    if response.code == 200
+      parse_data(response.body)
+    else
+      Rails.logger.error "Failed to fetch data for ISBN #{isbn}. HTTP Status: #{response.code}. "
+      nil
+    end
+  end
+
+  def self.parse_data(html)
+    doc = Nokogiri::HTML(html)
+
+    # Example: Extracting book Dewey Decimal Code
+    doc.search('//*[@class="fwikiItem divcanonicalddc"]/div/div/a')&.text&.strip
+  end
+end

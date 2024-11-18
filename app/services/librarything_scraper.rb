73347f09787518902ef0379732972b520adcd5d7
Mike Clancy <mclancy96@gmail.com>
2024-11-18 08:07:45 -0500

    html parser getting correct subdivision name

--- app/services/librarything_scraper.rb
+++ app/services/librarything_scraper.rb
@@ -3,49 +3,54 @@ require 'nokogiri'
 
 class LibrarythingScraper
   BASE_URL = 'https://www.librarything.com/mds/'.freeze
+  class << self
 
-
-  (0..1_000).each do |i|
-    
-  end
-
-  def convert_to_int_string(num)
-    "#{num.to_s.rjust(3, '0')}."
-  end
-
-  def add_decimal_place(num_string)
-    "#{num_string}0"
-  end
-
-  def add_one_to_decimal_place(num_string)
-    num_string[-1] == '9' ? nil : num_string[0...-1] + (num_string[-1].to_i + 1).to_s
-  end
-
-
-
-
-
-  def self.get_text(ddc)
-    url = "#{BASE_URL}#{ddc}"
-    sleep(2)
-    Rails.logger.info("ddc: #{ddc}")
-    response = HTTParty.get(url)
-
-    if response.code == 200
-      Rails.logger.info('success!')
-      parse_data(response.body)
-    else
-      Rails.logger.error "Failed to fetch data for ISBN #{isbn}. HTTP Status: #{response.code}. "
-      nil
+    def get_sub_divisions
+      subdivisions = []
+      (0..1_000).each do |i|
+        ddc_int_string = convert_to_int_string(i)
+        recursive_lookup(add_decimal_place(ddc_int_string), subdivisions)
+      end
     end
-  end
 
-  def self.parse_data(html)
-    doc = Nokogiri::HTML(html)
-    Rails.logger.info("parse_data: #{doc}")
-    # Example: Extracting book Dewey Decimal Code
-    ddc = doc.search('td[fieldname="modernschedule"] div div a')&.text&.strip
-    Rails.logger.info "Found #{ddc}"
-    ddc if ddc.present?
+    def get_text(ddc)
+      url = "#{BASE_URL}#{ddc}"
+      sleep(2)
+      Rails.logger.info("ddc: #{ddc}")
+      response = HTTParty.get(url)
+
+      if response.code == 200
+        Rails.logger.info('success!')
+        parse_data(response.body, ddc)
+      else
+        Rails.logger.error "Failed to fetch data for ISBN #{isbn}. HTTP Status: #{response.code}. "
+        nil
+      end
+    end
+
+    private
+
+    def recursive_lookup(ddc_string, collection_array)
+      ;
+    end
+
+    def convert_to_int_string(num)
+      "#{num.to_s.rjust(3, '0')}."
+    end
+
+    def add_decimal_place(num_string)
+      "#{num_string}0"
+    end
+
+    def add_one_to_decimal_place(num_string)
+      num_string[-1] == '9' ? nil : num_string[0...-1] + (num_string[-1].to_i + 1).to_s
+    end
+
+    def parse_data(html, ddc)
+      doc = Nokogiri::HTML(html)
+      ddc = doc.search("a[href='/mds/#{ddc}']")&.text&.strip&.slice(0...-1)
+      Rails.logger.info "Found #{ddc}"
+      ddc if ddc.present?
+    end
   end
 end

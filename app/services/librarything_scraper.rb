e080bb2650b2fbcb158200ec19f708d53e2ca6d7
Mike Clancy <mclancy96@gmail.com>
2024-11-18 12:09:36 -0500

    subdivision logic working so far

--- app/services/librarything_scraper.rb
+++ app/services/librarything_scraper.rb
@@ -5,20 +5,20 @@ class LibrarythingScraper
   BASE_URL = 'https://www.librarything.com/mds/'.freeze
   class << self
 
-    def get_sub_divisions
+    def sub_divisions
       subdivisions = []
-      (0..1_000).each do |i|
+      (0..1).each do |i|
+        Rails.logger.info "Getting subdivision #{i}"
         ddc_int_string = convert_to_int_string(i)
         recursive_lookup(add_decimal_place(ddc_int_string), subdivisions)
       end
+      subdivisions
     end
 
     def get_text(ddc)
       url = "#{BASE_URL}#{ddc}"
-      sleep(2)
       Rails.logger.info("ddc: #{ddc}")
       response = HTTParty.get(url)
-
       if response.code == 200
         Rails.logger.info('success!')
         parse_data(response.body, ddc)
@@ -31,7 +31,21 @@ class LibrarythingScraper
     private
 
     def recursive_lookup(ddc_string, collection_array)
-      ;
+      Rails.logger.info "Collection array currently: #{collection_array}"
+      return if ddc_string.nil?
+
+      subdivision_result = get_text(ddc_string)
+      unless subdivision_result.nil? || subdivision_result == 'Not set'
+        unless collection_array.include?(ddc_string)
+          collection_array << {
+            level: count_numerical_characters(ddc_string),
+            code: ddc_string,
+            description: subdivision_result,
+          }
+        end
+        recursive_lookup(add_decimal_place(ddc_string), collection_array)
+      end
+      recursive_lookup(add_one_to_decimal_place(ddc_string), collection_array)
     end
 
     def convert_to_int_string(num)
@@ -40,6 +54,10 @@ class LibrarythingScraper
 
     def add_decimal_place(num_string)
       "#{num_string}0"
+    end
+
+    def count_numerical_characters(str)
+      str.count('0-9')
     end
 
     def add_one_to_decimal_place(num_string)

4d555698f9234e335c69db899e3318098546cf09
Mike Clancy <mclancy96@gmail.com>
2024-11-19 16:58:36 -0500

    sql has too many apostrophes. use json

--- db/migrate/20241119203706_add_dewey_subdivisions.rb
+++ db/migrate/20241119203706_add_dewey_subdivisions.rb
@@ -1,10 +1,17 @@
 class AddDeweySubdivisions < ActiveRecord::Migration[7.1]
   def up
-    file_path = Rails.root.join('db', 'seeds', 'dewey_codes.sql')
+    # Read the JSON file
+    file_path = Rails.root.join('db', 'seeds', 'dewey_codes.json')
+    dewey_codes = JSON.parse(File.read(file_path))
 
-    sql_statements = File.read(file_path).split(";\n")
-    ActiveRecord::Base.transaction do
-      sql_statements.each { |statement| execute(statement) if statement.strip.present? }
+    # Loop through each DeweyCode and insert it if it doesn't already exist
+    dewey_codes.each do |dewey_code_data|
+      DeweyCode.find_or_create_by(id: dewey_code_data['id']) do |dewey_code|
+        dewey_code.level = dewey_code_data['level']
+        dewey_code.code = dewey_code_data['code']
+        dewey_code.description = dewey_code_data['description']
+        # created_at and updated_at will be handled automatically by Rails
+      end
     end
   end
 

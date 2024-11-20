7018d654c7d3f172afa7881fb5927590a8607150
Mike Clancy <mclancy96@gmail.com>
2024-11-19 20:00:58 -0500

    add parent and child codes

--- app/models/dewey_code.rb
+++ app/models/dewey_code.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class DeweyCode < ApplicationRecord
+  def parent
+    return nil if level <= 1
 
+    parent_level = level - 1
+    parent_code = code[-2] == '.' ? code[0...-2] : code[0...-1]
+    DeweyCode.find_by(level: parent_level, code: parent_code)
+  end
+
+  def children
+    DeweyCode.where('code LIKE ?', "#{code}%").and(DeweyCode.where('level = ? + 1', "#{level}%"))
+  end
 end

306be19ccc5fd0d1ca8d4fd5ce7f48a364deaeae
Mike Clancy <mclancy96@gmail.com>
2024-11-20 14:58:35 -0500

    fix migration

--- db/migrate/20241120010858_add_dewey_code_to_books.rb
+++ db/migrate/20241120010858_add_dewey_code_to_books.rb
@@ -1,7 +1,7 @@
 class AddDeweyCodeToBooks < ActiveRecord::Migration[7.1]
   def change
     rename_column :books, :ddc, :dewey_code_id
-    change_column :books, :dewey_code_id, :bigint
+    change_column :books, :dewey_code_id, :bigint, using: 'dewey_code_id::bigint'
     add_foreign_key :books, :dewey_codes
     add_index :books, :dewey_code_id
   end

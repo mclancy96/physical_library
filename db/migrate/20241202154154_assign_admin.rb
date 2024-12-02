a13c2ac90960b4af736dfcd8f32b8fee44965640
Mike Clancy <mclancy96@gmail.com>
2024-12-02 10:52:48 -0500

    use code instead of name

--- db/migrate/20241202154154_assign_admin.rb
+++ db/migrate/20241202154154_assign_admin.rb
@@ -1,9 +1,9 @@
 class AssignAdmin < ActiveRecord::Migration[7.1]
   def change
-    admin = Role.find_by(name: 'admin')
+    admin = Role.find_by(code: 'admin')
     lilli = Member.where('name like ?', '%lilli%').first
     mike = Member.where('name like ?', '%Mike%').first
-    lilli&.update_attribute(:role, admin.id)
-    mike&.update_attribute(:role, admin.id)
+    lilli&.update_attribute(:role, admin)
+    mike&.update_attribute(:role, admin)
   end
 end
